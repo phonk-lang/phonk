1730d7029cd3a3b62f7c689c86f934a90597f2fb
Jasio Adorno <jasioadorno@proton.me>
2026-05-22 10:18:24 -0400

    More Syntax Corrections
    I hate semicolons and case sensitivity

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
+#include<string>
+#include<unordered_set>
+
+
 
 enum class tolkentype {
     INT, FLOAT, STRING, KEYWORD, IDENTIFIER, PLUS, MINUS, L_PAREN, R_PAREN, L_BRACE, R_BRACE, L_BRACKET, R_BRACKET, SEMICOLON, COMMA, DOT, COLON, NEWLINE, UNKNOWN, EOF_TOKEN
-}
+};
 
 struct Token{
 tolkentype type;
 std::string value;
 int line;
-}
+};
 
-Class Lexer{
+class Lexer{
 public:
-    Lexer(const std::string&source):Source_(source),pos(0),line(1){
-    keywords_ = {"int", "float", "string", "bool", "if", "else", "for","return", "true", "false", "print"}}
+    Lexer(const std::string&source_):source_(source_),pos_(0),line_(1){
+    keywords_ = {"int", "float", "string", "bool", "if", "else", "for", "return", "true", "false", "print"};}
     
 private:
- std::string source;
- size_t pos;
- int line;
- std::unordered_set<std::string> keywords;
- char c;
+ std::string source_;
+ size_t pos_;
+ int line_;
+ std::unordered_set<std::string> keywords_;
+ char c = '\0';
  
 void skipwhitespace(){
-    while(pos<source.size()){
-        c = source[pos];
+    while(pos_<source_.size()){
+        c = source_[pos_];
         if(c == ' ' || c == '\t'){
-          pos++;}
+          pos_++;}
         else{
             break;}}}
             
 void skipblankline(){
 
-    while(pos<source.size()){
-    c = source[pos];
-    if (c == '\n')
-        line++
-    else 
+    while(pos_<source_.size()){
+    c = source_[pos_];
+    if (c == '\n'){
+        line_++;
+        pos_++;}
+    else {
         break;}}
 
 void skipcomment(){
-    if(c == '/' && pos + 1 < source.size() && source[pos + 1] == '/'){
-        pos += 2;
-        while(pos < source.size() && source[pos] != '\n')
-            pos++;}}
+    if(c == '/' && pos_ + 1 < source_.size() && source_[pos_ + 1] == '/'){
+        pos_ += 2;
+        while(pos_ < source_.size() && source_[pos_] != '\n')
+            pos_++;}}
 
 void checknext(){
-    if(pos < source.size()){
-        c = source[pos++];}
+    if(pos_ < source_.size()){
+        c = source_[pos_++];}
     else{
         c = '\0';}}
+};
